757b1a95e5344751c6c81c12effdde371cc4193b
thor <thor@cphbusiness.dk>
2022-11-04 14:49:55 +0100

    outerloop for next but

--- bilbasen.R
+++ bilbasen.R
@@ -4,50 +4,57 @@ library(dplyr)
 
 #selenium-stuff
 startlink <- 'https://www.bilbasen.dk/brugt/bil?IncludeEngrosCVR=true&PriceFrom=0&includeLeasing=false'
-#rD <- rsDriver(port = 4589L, browser = c('chrome'))
-rD <- rsDriver(port = 4590L, browser = c('firefox'))
+rD <- rsDriver(port = 4591L, browser = c('firefox'))
 rclient <- rD[['client']]
 rclient$navigate(startlink)
-pagesource <- rclient$getPageSource()
 
 #rvest
 maincartag=".bb-listing-clickable"
-carhtml <- read_html(pagesource[[1]])
-carlist <- carhtml %>% html_nodes(maincartag)
-testcar = carlist[[3]]
+#carhtml <- read_html(pagesource[[1]])
+#carlist <- carhtml %>% html_nodes(maincartag)
 
 resdf = as.data.frame(matrix(nrow = 0, ncol = 7))
 names=c("mpg","milage","maketype","id","region","year","price")
 colnames(resdf) <- names
+maxlimit=3
 
-for (car in carlist) {
-  tmpdf = as.data.frame(matrix(nrow = 1, ncol = 7))
-  colnames(tmpdf) <- names
-  
-  #get idtag
-  carid <- getcarid(car)
-  tmpdf$id=carid
-  
-  #get price
-  price <- getcarprice(car)
-  tmpdf$price=price
-  
-  # get mpg, milage, year
-  infolist <- getinfolist(car)
-  tmpdf$year=infolist[4]
-  tmpdf$milage=infolist[3]
-  tmpdf$mpg=infolist[2]
-  
-  # get maketype-info
-  maketypetag = ".darkLink"
-  mklist <-  car %>% html_nodes(maketypetag) %>% html_text()
-  tmpdf$maketype=mklist
-  
-  # get region
-  regtag = ".listing-region"
-  region <-  car %>% html_nodes(regtag) %>% html_text()
-  tmpdf$region=region
-  resdf <- rbind(resdf,tmpdf)
+for (counter in (1:maxlimit)) {
+  pagesource <- rclient$getPageSource()
+  carhtml <- read_html(pagesource[[1]])
+  carlist <- carhtml %>% html_nodes(maincartag)
+
+  for (car in carlist) {
+    tmpdf = as.data.frame(matrix(nrow = 1, ncol = 7))
+    colnames(tmpdf) <- names
+    
+    #get idtag
+    carid <- getcarid(car)
+    tmpdf$id=carid
+    
+    #get price
+    price <- getcarprice(car)
+    tmpdf$price=price
+    
+    # get mpg, milage, year
+    infolist <- getinfolist(car)
+    tmpdf$year=infolist[4]
+    tmpdf$milage=infolist[3]
+    tmpdf$mpg=infolist[2]
+    
+    # get maketype-info
+    maketypetag = ".darkLink"
+    mklist <-  car %>% html_nodes(maketypetag) %>% html_text()
+    tmpdf$maketype=mklist
+    
+    # get region
+    regtag = ".listing-region"
+    region <-  car %>% html_nodes(regtag) %>% html_text()
+    tmpdf$region=region
+    resdf <- rbind(resdf,tmpdfd)
+  }
+  button=rclient$findElement(using = "class name","next")
+  button$clickElement()
+  Sys.sleep(5)
 }
 
 
